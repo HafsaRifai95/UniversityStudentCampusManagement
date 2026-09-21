506867ea57c6f1f330b5683a735a4de3ac8b545b
hafsarifai <hafsarifai95@gmail.com>
2026-09-21 21:00:17 +0530

    Graph implementation

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -10,10 +10,12 @@ public class Graph {
         adjacencyList = new HashMap<>();
     }
 
+    // Add a campus location
     public void addVertex(int vertex) {
         adjacencyList.putIfAbsent(vertex, new ArrayList<>());
     }
 
+    // Add a connection/road
     public void addEdge(int vertex1, int vertex2) {
         addVertex(vertex1);
         addVertex(vertex2);
@@ -22,9 +24,37 @@ public class Graph {
         adjacencyList.get(vertex2).add(vertex1);
     }
 
+    // Remove a connection/road
+    public void removeEdge(int vertex1, int vertex2) {
+        if (adjacencyList.containsKey(vertex1) &&
+            adjacencyList.containsKey(vertex2)) {
+
+            adjacencyList.get(vertex1).remove(Integer.valueOf(vertex2));
+            adjacencyList.get(vertex2).remove(Integer.valueOf(vertex1));
+        }
+    }
+
+    // Remove a campus location
+    public void removeVertex(int vertex) {
+        if (adjacencyList.containsKey(vertex)) {
+
+            for (List<Integer> neighbours : adjacencyList.values()) {
+                neighbours.remove(Integer.valueOf(vertex));
+            }
+
+            adjacencyList.remove(vertex);
+        }
+    }
+
+    // Display the campus network
     public void displayGraph() {
         for (int vertex : adjacencyList.keySet()) {
             System.out.println(vertex + " -> " + adjacencyList.get(vertex));
         }
+    }
+
+    // Get the adjacency list for BFS and DFS
+    public Map<Integer, List<Integer>> getAdjacencyList() {
+        return adjacencyList;
     }
 }
